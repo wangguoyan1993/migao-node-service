cf5bab7c3adc66d510b691b1ae00099cd0e4b311
wangguoyan <2282940593@qq.com>
2020-06-11 23:09:18 +0800

    - 添加ts-node debug模式

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 let createError = require('http-errors');
-let express = require('express');
+let Express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
@@ -7,35 +7,35 @@ let logger = require('morgan');
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
-let app = express();
+let application = Express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+application.set('views', path.join(__dirname, 'views'));
+application.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+application.use(logger('dev'));
+application.use(Express.json());
+application.use(Express.urlencoded({ extended: false }));
+application.use(cookieParser());
+application.use(Express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+application.use('/', indexRouter);
+application.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+application.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+application.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.application.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+module.exports = application;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-let express = require('express');
-let router = express.Router();
+import express = require('express');
+let Router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+Router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = Router;
